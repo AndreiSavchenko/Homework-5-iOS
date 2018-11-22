114916e6e4cad7c236163ec1e810d6be3992a3eb
AndreiSavchenko <andrei.savchenko.ad@gmail.com>
2018-11-22 18:34:39 +0200

    Add HW 5

---
+++ Homework-5-iOS/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Homework-5-iOS
+//
+//  Created by Alla on 11/21/18.
+//  Copyright © 2018 AndreiSavchenko. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Homework-5-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework-5-iOS
+//
+//  Created by Alla on 11/21/18.
+//  Copyright © 2018 AndreiSavchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
